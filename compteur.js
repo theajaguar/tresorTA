6c04bdb7c76828f8458bea38606134c5687383e5
thea <theaordi@gmail.com>
2025-12-21 18:18:02 +0100

    Update compteur.js

--- compteur.js
+++ compteur.js
@@ -1,9 +1,10 @@
 const contributions = [
-  { amount: 630, date: new Date("2025-07-06T00:00:00Z") },
+  { amount: 350, date: new Date("2025-07-06T00:00:00Z") },
   { amount: 50,  date: new Date("2025-08-16T00:00:00Z") },
   { amount: 50,  date: new Date("2025-09-02T00:00:00Z") },
   { amount: 50,  date: new Date("2025-09-26T00:00:00Z") },
-  { amount: 200, date: new Date("2025-12-12T00:00:00Z") }
+  { amount: 200, date: new Date("2025-12-12T00:00:00Z") },
+  { amount: 250, date: new Date("2025-12-21T00:00:00Z") }
 ];
 
 const interestRate = 0.02;
